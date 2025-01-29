62663682fb0a2468e860ca753fa456a8beeaae53
jeloFojas <jelo.fojas@deped.gov.ph>
2025-01-29 06:23:53 +0000

    commit

--- main.cs
+++ main.cs
@@ -2,6 +2,29 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.WriteLine ("=====     AGE GROUP CLASSIFIER     =====");
+    Console.WriteLine ("========================================");
+    Console.WriteLine ("");
+
+    Console.Write("Enter your name: ");
+    string nameInput = Console.ReadLine();
+
+    Console.Write("Enter your age: ");
+    int ageInput = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine ("========================================");
+    Console.WriteLine ("");
+
+    Console.WriteLine($"User's Name: {nameInput}");
+    Console.WriteLine($"User's Age: {ageInput} years old");
+
+    if (ageInput > 59) {
+      Console.WriteLine("You are a Senior Citizen"); 
+    } else if (ageInput > 19){
+      Console.WriteLine("You are an adult"); 
+    } else if (ageInput > 12) {
+      Console.WriteLine( "You are a teenager");
+    } else {
+      Console.WriteLine("You are a child");
+    }
   }
 }
